4fc114f4fe0f75b2f5ee5575c3bf78e1a8625d30
Emre Akman <emre@oblieapps.com>
2017-11-27 10:20:16 +0100

    Read size increase. Removed read params.

--- Sources/TCPClient.swift
+++ Sources/TCPClient.swift
@@ -125,15 +125,15 @@ open class TCPClient: Socket {
     * read data with expect length
     * return success or fail with message
     */
-    open func read(_ expectlen:Int, timeout:Int = -1) -> [Byte]? {
+    open func read() -> [Byte]? {
         guard let fd:Int32 = self.fd else { return nil }
-      
-        var buff = [Byte](repeating: 0x0,count: expectlen)
-        let readLen = c_ytcpsocket_pull(fd, buff: &buff, len: Int32(expectlen), timeout: Int32(timeout))
+        
+        var buff = [Byte](repeating: 0x0,count: 9999999)
+        let readLen = c_ytcpsocket_pull(fd, buff: &buff, len: Int32(9999999), timeout: Int32(-1))
         if readLen <= 0 { return nil }
         let rs = buff[0...Int(readLen-1)]
         let data: [Byte] = Array(rs)
-      
+        
         return data
     }
 }
